e8d98af445600a4255ce2bcc5c28468867163ba2
denfedweb <denfedweb@mail.ru>
2020-05-09 11:05:03 +0300

    added components, and added event logic

---
+++ src/core/DomListener.js
@@ -0,0 +1,33 @@
+import {capitalize} from "./utils";
+
+export class DomListener {
+    constructor($root, listeners = []){
+        if (!$root) {
+            throw Error("No $root provided for DomListener!");
+        }
+        this.$root = $root;
+        this.listeners = listeners;
+    }
+
+    initDomListeners(){
+        
+        this.listeners.forEach(listener => {
+            const method = getMethodName(listener);
+            if (!this[method]){
+                throw new Error(`Method ${method} is not
+                 implemented in ${this.name} Component`);
+            }
+            // addEventListener("");
+            this.$root.on(listener, this[method].bind(this));
+
+        });
+    }
+
+    removeDomListeners(){
+
+    }
+
+}
+
+// input => onInput
+const getMethodName = eventName => `on${capitalize(eventName)}`;
